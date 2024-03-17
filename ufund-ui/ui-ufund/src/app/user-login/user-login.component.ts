f668b89bf63bd67fa6b29fcf136170e64ad925ba
nsh1507 <nsh1507@g.rit.edu>
2024-03-16 21:56:56 -0400

    add a login and signup page to handle different request

--- ufund-ui/ui-ufund/src/app/user-login/user-login.component.ts
+++ ufund-ui/ui-ufund/src/app/user-login/user-login.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class UserLoginComponent {
 
+  signUp: boolean = true;
+  signUpObj: SignUpModel = new SignUpModel();
+  logInObj: LoginModel = new LoginModel();
+
+  onSignIn(){
+    const localUser = localStorage.getItem("angular17");
+
+    if (localUser != null){
+      const users = JSON.parse(localUser)
+      users.push(this.signUpObj);
+      localStorage.setItem("angular17", JSON.stringify(users));
+    } 
+    else {
+      const users = [];
+      users.push(this.signUpObj);
+      localStorage.setItem("angular17", JSON.stringify(users));
+    }
+  }
+}
+
+export class SignUpModel{
+  username: string;
+  password: string
+
+  constructor() {
+    this.username = "";
+    this.password = "";
+  }
+}
+
+export class LoginModel{
+  username: string;
+  password: string
+
+  constructor() {
+    this.username = "";
+    this.password = "";
+  }
 }
