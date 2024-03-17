d932d62ec597bad3dc7b02c271638473fc1c2a70
nsh1507 <nsh1507@g.rit.edu>
2024-03-17 00:13:39 -0400

    fine tuned user-login

--- ufund-ui/ui-ufund/src/app/user-login/user-login.component.ts
+++ ufund-ui/ui-ufund/src/app/user-login/user-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login',
@@ -10,6 +11,8 @@ export class UserLoginComponent {
   signUp: boolean = true;
   signUpObj: SignUpModel = new SignUpModel();
   logInObj: LoginModel = new LoginModel();
+
+  constructor(private router: Router){}
 
   onSignIn(){
     const localUser = localStorage.getItem("angular17");
@@ -24,7 +27,29 @@ export class UserLoginComponent {
       users.push(this.signUpObj);
       localStorage.setItem("angular17", JSON.stringify(users));
     }
+    this.signUp = false;
+    alert("Registration Successful!");
   }
+
+  
+  onLogIn(){
+    const localUser = localStorage.getItem("angular17");
+
+    if (localUser != null){
+      const users = JSON.parse(localUser)
+      
+      const isUserExist = users.find( (user:SignUpModel)=> user.username == this.logInObj.username && this.logInObj.password);
+      if (isUserExist != undefined){
+        alert("User Found!"); 
+        localStorage.setItem('loggedUser', JSON.stringify(isUserExist));
+        this.router.navigateByUrl('/dashboard');
+      } 
+      else {
+        alert("No User Found!"); 
+      }
+    }
+  }
+
 }
 
 export class SignUpModel{
